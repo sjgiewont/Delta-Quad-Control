02e4704b444cce859aa7d117037b9c7ac20f1f00
Steven Giewont <Steven Giewont>
2017-05-03 22:56:38 -0400

    New ip address

--- socket_test.py
+++ socket_test.py
@@ -49,7 +49,7 @@ serversocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
 # define port and host address
 PORT = 12345
-HOST = '129.21.90.103'
+HOST = '129.21.89.120'
 
 serversocket.bind((HOST, PORT))
 serversocket.listen(10)
